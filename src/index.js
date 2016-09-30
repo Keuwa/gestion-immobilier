1997b8506e528d571fff2376367b870247b935ae
Durpoix <vincent.durpoix@gmail.com>
2016-09-30 17:49:40 +0200

    start react

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ var app = require('./server');
 var api = require('./api');
 var mongoose = require('mongoose');
 var settings = require('./api/settings');
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import { match, RouterContext } from 'react-router';
+import routes from './routes';
 var _ = require('lodash');
 var env = process.env.NODE_ENV;
 var server = require('http').Server(app);
@@ -15,9 +19,43 @@ else {
 }
 
 // Home request API
-app.get('/', function (req, res) {
+/*app.get('/', function (req, res) {
   res.send('Welcome to gestion-immobilier API ! <br/><br/>-Go to /api to Show all route of the API<br/><br/>-Go to /doc to see the API documentation');
+});*/
+
+// universal routing and rendering
+app.get('/', (req, res) => {
+  match(
+    { routes, location: req.url },
+    (err, redirectLocation, renderProps) => {
+
+      // in case of error display the error message
+      if (err) {
+        return res.status(500).send(err.message);
+      }
+
+      // in case of redirect propagate the redirect to the browser
+      if (redirectLocation) {
+        return res.redirect(302, redirectLocation.pathname + redirectLocation.search);
+      }
+
+      // generate the React markup for the current route
+      let markup;
+      if (renderProps) {
+        // if the current route matched we have renderProps
+        markup = renderToString(<RouterContext {...renderProps}/>);
+      } else {
+        // otherwise we can render a 404 page
+        /*markup = renderToString(<NotFoundPage/>);*/
+        res.status(404);
+      }
+
+      // render the index template with the embedded React markup
+      //return res.render('index', { markup });
+    }
+  );
 });
+
 
 // user API router
 app.use('/api', api);
